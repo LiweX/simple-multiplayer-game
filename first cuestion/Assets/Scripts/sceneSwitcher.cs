0de720163ba6c9aa72a71b9bba57ee2a5955b6c7
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-08-20 03:19:37 -0300

    TCP client mode not working on webgl :(

--- first cuestion/Assets/Scripts/sceneSwitcher.cs
+++ first cuestion/Assets/Scripts/sceneSwitcher.cs
@@ -3,52 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using HybridWebSocket;
 
 public class sceneSwitcher : MonoBehaviour
 {
     private inputReader ipObject;
     private string ip;
-    public void ChangeScene(int sceneID){
+
+    public void tryConnect(int sceneID){
 
         getIPfromInputField();
         Debug.Log(ip);
-
-        // Create WebSocket instance
-        WebSocket ws = WebSocketFactory.CreateInstance("ws://" + ip + ":8001");
-
-        // EVENT LISTENERS
-        ws.OnOpen += () =>
-        {
-            Debug.Log("WS connected!");
-            Debug.Log("WS state: " + ws.GetState().ToString());
-
-            ws.Send(Encoding.UTF8.GetBytes("Hello from Unity 3D!"));
-
-            
-            
-        };
-        ws.OnMessage += (byte[] msg) =>
-        {
-            Debug.Log("WS received message: " + Encoding.UTF8.GetString(msg));
-        };
-
-        ws.OnError += (string errMsg) =>
-        {
-            Debug.Log("WS error: " + errMsg);
-        };
-
-        ws.OnClose += (WebSocketCloseCode code) =>
-        {
-            Debug.Log("WS closed with code: " + code.ToString());
-        };
-
-        // Connect to the server
-        ws.Connect();
-
-        SceneManager.LoadScene(1);
-
-        
     }
 
     public void getIPfromInputField(){
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-import asyncio
-import websockets
+import socket
 
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        await websocket.send("Hello from Python")
+ip = "127.0.0.1"
+port = 8001
 
-async def main():
-    async with websockets.serve(echo, "localhost", 8001):
-        await asyncio.Future()  # run forever
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((ip,port))
+server.listen(5)
+client, address = server.accept()
+print(f"Connection Established - {address[0]}:{address[1]}")
 
-asyncio.run(main())
+while True:
+    string = client.recv(1024)
+    string = string.decode("utf-8")
+    print(string)
+    client.send(bytes("hello from python","utf-8"))
